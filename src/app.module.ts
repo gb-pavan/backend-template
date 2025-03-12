5d7b8c3b2b63a73ec8fdc61f70b79246a8412979
pavan <pavankmr1902@gmail.com>
2025-03-12 14:40:07 +0530

    integrated prisma to nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,38 @@
-import { Module, OnModuleInit } from '@nestjs/common';
+// import { Module, OnModuleInit } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { MongoModule } from '@modules/database/mongo.module';
+// import * as mongoose from 'mongoose';
+// import { PrismaService } from '@modules/prisma/prisma.service';
+
+// @Module({
+//   imports: [MongoModule],
+//   exports: [PrismaService],
+//   controllers: [AppController],
+//   providers: [AppService,PrismaService],
+// })
+// export class AppModule implements OnModuleInit {
+//   async onModuleInit() {
+//     try {
+//       // This will wait for the connection to succeed or fail
+//       await mongoose.connect('mongodb://root:rootpassword@localhost:27017/');
+//       console.log('MongoDB successfully connected');
+//     } catch (error) {
+//       console.error('Error connecting to MongoDB:', error);
+//     }
+//   }
+// }
+
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongoModule } from '@modules/database/mongo.module';
-import * as mongoose from 'mongoose';
+import { PrismaService } from '@modules/prisma/prisma.service';
 
 @Module({
-  imports: [MongoModule],
+  imports: [], // Removed MongoModule since it's related to Mongoose
+  exports: [PrismaService],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
-export class AppModule implements OnModuleInit {
-  async onModuleInit() {
-    try {
-      // This will wait for the connection to succeed or fail
-      await mongoose.connect('mongodb://root:rootpassword@localhost:27017/');
-      console.log('MongoDB successfully connected');
-    } catch (error) {
-      console.error('Error connecting to MongoDB:', error);
-    }
-  }
-}
+export class AppModule {}
+
